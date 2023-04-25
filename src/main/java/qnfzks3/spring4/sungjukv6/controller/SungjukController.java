38745b3fcf7d0ea3185ceddbdff0112cff826850
Lea Sae Cheul <dltpcjf1005@naver.com>
2023-04-25 15:48:17 +0900

    스프링mvc 프로젝트 - 성적조회2

--- src/main/java/qnfzks3/spring4/sungjukv6/controller/SungjukController.java
+++ src/main/java/qnfzks3/spring4/sungjukv6/controller/SungjukController.java
@@ -3,26 +3,33 @@ package qnfzks3.spring4.sungjukv6.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 import qnfzks3.spring4.sungjukv6.model.SungJukVO;
 import qnfzks3.spring4.sungjukv6.service.SungJukV6Service;
+
+
 
 
 @Controller
 public class SungjukController {
     private SungJukV6Service sjsrv;
 
-    @Autowired
+    @Autowired   //@Autowired는 해당 클래스가 의존하고 있는 다른 객체를 찾아서 자동으로 주입
     public SungjukController(SungJukV6Service sjsrv) {
         this.sjsrv = sjsrv;
     }
 
-    @RequestMapping(value = "/list",method = RequestMethod.GET)
-    public String list(){
-        System.out.println(sjsrv.readSungJuk());
-        return "sungjuklist";
+    @RequestMapping(value = "/list",method = RequestMethod.GET) //@RequestMapping은 value로 url패턴을 지정하고 http에 get이나 post로 지정한다.-
+    public String list(Model model){                                       //@RequestMapping는  HTTP 요청을 처리하고, 결과를 응답합니다.
 
+        System.out.println(sjsrv.readSungJuk());
+
+        model.addAttribute("sjs",sjsrv.readSungJuk());//sungjuklist.jsp에 성정 조회결과 데이터를  sjs에 넣는다.
+        return "sungjuklist";                  // 이 데이터를 sjs에 넣는 걸 sungjuklist 로 보낸다. - return으로 sungjuklist에서도 사용가능하게 함
+                                                //추천 책 읽어봐도된다.
     }
 
 }
---
+++ src/main/webapp/WEB-INF/views/sungjuklist.jsp
@@ -0,0 +1,49 @@
+<%--
+ 내부적으로 뭔가를 만들고 나서 보여주는 역할
+ 기본적으로 model, controller, service , dao 를 각각 있어야한다.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>  <%--테크 라이브러리를 넣을수있게--%>
+                                                                <%--<dependency>
+                                                                    <groupId>javax.servlet</groupId>
+                                                                    <artifactId>jstl</artifactId>
+                                                                    <version>1.2</version>
+
+                                                                </dependency>
+                                                                이걸 porm에 추가해준다.--%>
+
+
+<html>
+<head>
+    <title>성적 리스트</title>
+</head>
+<body>
+    <h1>성적 리스트</h1>
+    <table border="1" width="750px" cellpadding="10px" cellspacing="0">
+        <tr>
+            <th>이름</th>
+            <th>국어</th>
+            <th>영어</th>
+            <th>수학</th>
+        </tr>
+        <%--  <c: 하고 컨트롤 스페이스하면 사용할수있는 구문이 다나온다.--%>
+        <%--for(SungJuk sj : sjs) 와 같다. sj의 타입은 안적어도 된다.--%>
+        <c:forEach items="${sjs}" var="sj">
+            <tr>
+                <td>${sj.name}</td>
+                <td>${sj.kor}</td>
+                <td>${sj.eng}</td>
+                <td>${sj.mat}</td>
+            </tr>
+
+        </c:forEach>
+
+    </table>
+
+
+
+
+
+
+</body>
+</html>
