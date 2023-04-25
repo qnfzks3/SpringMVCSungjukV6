9bc2fe10aa3d83fad5c69de341ef1cde68bc204a
Lea Sae Cheul <dltpcjf1005@naver.com>
2023-04-25 16:08:48 +0900

    스프링mvc 프로젝트 - 성적조회3

--- src/main/java/qnfzks3/spring4/sungjukv6/controller/SungjukController.java
+++ src/main/java/qnfzks3/spring4/sungjukv6/controller/SungjukController.java
@@ -4,6 +4,7 @@ package qnfzks3.spring4.sungjukv6.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -22,14 +23,29 @@ public class SungjukController {
         this.sjsrv = sjsrv;
     }
 
+    /*
     @RequestMapping(value = "/list",method = RequestMethod.GET) //@RequestMapping은 value로 url패턴을 지정하고 http에 get이나 post로 지정한다.-
-    public String list(Model model){                                       //@RequestMapping는  HTTP 요청을 처리하고, 결과를 응답합니다.
+    public String list(Model model){ //모델에 대한 생성자가 알아서 Model을 해줌-보이지 않을뿐 //@RequestMapping는  HTTP 요청을 처리하고, 결과를 응답합니다.
 
         System.out.println(sjsrv.readSungJuk());
 
         model.addAttribute("sjs",sjsrv.readSungJuk());//sungjuklist.jsp에 성정 조회결과 데이터를  sjs에 넣는다.
         return "sungjuklist";                  // 이 데이터를 sjs에 넣는 걸 sungjuklist 로 보낸다. - return으로 sungjuklist에서도 사용가능하게 함
                                                 //추천 책 읽어봐도된다.
+                                                */
+    @GetMapping(value = "/list")            //이렇게 모델엔뷰 로 만들어서 간편하게 할 수 있다.
+    public ModelAndView list(){
+        ModelAndView mv = new ModelAndView();
+
+        //Sungjuklist.jsp에 성적 조회 결과를 sjs라는 이름으로 넘김
+        mv.addObject("sjs",sjsrv.readSungJuk());
+        mv.setViewName("sungjuklist");  //뷰 이름 지정
+        return mv;
+
+
+
+
+
     }
 
 }
